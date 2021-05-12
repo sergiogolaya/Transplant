a7323bdeb528242381e3ec0e449c705df0314b86
carlotasqs <c.sanchez16@ceu.usp.es>
2021-05-12 17:10:46 +0200

    MENUJAVA

--- src/transplant/ui/Menu.java
+++ src/transplant/ui/Menu.java
@@ -5,6 +5,7 @@ import transplant.db.ifaces.DBManager;
 import transplant.db.jdbc.JDBCManager;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.format.DateTimeFormatter;
 import java.sql.Date;
@@ -22,14 +23,94 @@ import transplant.db.pojos.Patient;
 public class Menu {
 	public static DBManager dbman = new JDBCManager();
 	private static DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	 public static int imprimirMenuHospital() {
+		int opc=0;
+		try {
+		BufferedReader console= new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("\n\nWELCOME TO SECOND LIFE\n\n");
+		System.out.print("\n1.Add request");
+		System.out.print("\n2.Check request");
+		System.out.print("\nIntroduce an option\n");
+		opc=Integer.parseInt(console.readLine());
+		
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return opc;
+		
+	}
+	
+	void imprimirMenuDP() {
+		System.out.print("\n\nWELCOME TO SECOND LIFE\n\n");
+		System.out.print("\n1.Check Request");
+		System.out.print("\n3.Exit");
+	
+	}
+	public static int imprimeMenu1Hospital() {
+		//ADD REQUEST 
+		int option=0;
+		System.out.print("\nADD SECTION");
+		System.out.print("\n1.Add patient");
+		System.out.print("\n2.Add donor");
+		System.out.print("\n3.Exit");
+		System.out.print("\nIntroduce an option\n");
+		try {
+			BufferedReader console= new BufferedReader(new InputStreamReader(System.in));
+			option=Integer.parseInt(console.readLine());
+			
+			}catch(IOException e) {
+				e.printStackTrace();
+			
+			}
+		return option;
+	
+	}
+	public static int imprimeMenu2Hospital() {
+		//CHECK REQUEST 
+		int option=0;
+		System.out.print("\nCHECK REQUEST SECTION");
+		System.out.print("\n1.Delete request");
+		System.out.print("\n2.Modify request");
+		System.out.print("\n3.Exit");
+		System.out.print("\nIntroduce an option\n");
+		try {
+			BufferedReader console= new BufferedReader(new InputStreamReader(System.in));
+			option=Integer.parseInt(console.readLine());
+			
+			}catch(IOException e) {
+				e.printStackTrace();
+			
+			}
+		return option;
+	
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
-		dbman.connect();
-		//addPatient();
-		//addDonor();
-		//addHospital();
-		//addDonation();
-		addMedicalHistory();
-		dbman.disconnect();
+		
+		//HOSPITAL MENU
+		int opc=imprimirMenuHospital();
+		switch(opc) {
+		case 1:  
+			int opc2=imprimeMenu1Hospital();
+			if(opc2==1)  addPatient();
+			else if(opc2==2)  addDonor();
+			else if(opc2==3);//Exit(0);
+			break;
+		case 2: 
+			int opc3=imprimeMenu2Hospital();
+			if (opc3==1);//remove requers
+			else if (opc3==2);//modify r
+			else if (opc3==3);//exit
+			break;
+		}
+			
+			
+			
+		
+		
+		
+		
 		}
 private static void addPatient() throws Exception{
 	
